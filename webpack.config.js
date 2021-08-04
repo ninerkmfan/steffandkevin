e6640bae24fd07c03428b8323ee51e98302df8cb
Kevin <metz.kevin@gmail.com>
2021-08-04 17:26:59 -0400

    added mobile menu, pancake icon, and transition effects on header

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,18 @@ module.exports = {
     entry: './app/assets/scripts/App.js',
     output: {
         filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
+        path: path.resolve(__dirname)
+    },
+    devServer: {
+        before: function (app, server) {
+            server._watch('*.html')
+        },
+        contentBase: path.join(__dirname),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
